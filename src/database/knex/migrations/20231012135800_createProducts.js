3b41ea03bdad49ac4e3e9965ffa21e04dbf018ee
rayaneacacio <rray74307@gmail.com>
2023-10-17 10:14:42 -0300

    atualizacao do banco de dados

--- src/database/knex/migrations/20231012135800_createProducts.js
+++ src/database/knex/migrations/20231012135800_createProducts.js
@@ -1,7 +1,6 @@
 exports.up = knex => knex.schema.createTable("products", table => {
   table.increments("id");
   table.text("name");
-  table.text("size");
   table.text("category");
   table.integer("price");
   table.integer("promotion");
--- src/database/knex/migrations/20231016152548_create_products_images.js
+++ src/database/knex/migrations/20231016152548_create_products_images.js
@@ -1,7 +1,7 @@
 exports.up = knex => knex.schema.createTable("products_images", table => {
   table.increments("id");
   table.integer("product_id").references("id").inTable("products").onDelete("CASCADE");
-  table.integer("color_id").references("id").inTable("colors").onDelete("CASCADE");
+  table.integer("color_id").references("id").inTable("products_colors").onDelete("CASCADE");
   table.text("image");
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
