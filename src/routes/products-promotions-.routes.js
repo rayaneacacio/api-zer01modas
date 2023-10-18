52542cfd8d8f051be5bfeaf8b73bafd65c7d85db
rayaneacacio <rray74307@gmail.com>
2023-10-18 12:04:12 -0300

    adicionando middleware de autenticacao nas rotas de promotions

--- src/routes/products-promotions-.routes.js
+++ src/routes/products-promotions-.routes.js
@@ -3,7 +3,11 @@ const { Router } = require("express");
 const ProductsPromotionsController = require("../controllers/products-promotions-controller");
 const productsPromotionsController = new ProductsPromotionsController();
 
+const EnsureAuthenticated = require("../middlewares/ensureAuthenticated");
+
 const productsPromotionsRoutes = Router();
+
+productsPromotionsRoutes.use(EnsureAuthenticated);
 
 productsPromotionsRoutes.post("/", productsPromotionsController.create);
 productsPromotionsRoutes.get("/index", productsPromotionsController.index);
