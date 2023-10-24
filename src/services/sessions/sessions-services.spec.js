630b7fd6e9542cde5ade95ddd35e7a886800d5bc
rayaneacacio <rray74307@gmail.com>
2023-10-24 13:52:12 -0300

    criacao de testes automatizados sessions-services.spec.js

---
+++ src/services/sessions/sessions-services.spec.js
@@ -0,0 +1,10 @@
+const SessionsServices = require("./sessions-services");
+const AppError = require("../../utils/AppError");
+
+describe("SessionsServices", () => {
+  it("verifica se o user existe", async() => {
+    const sessionsServices = new SessionsServices();
+    
+    expect(sessionsServices.verifyUserExists()).rejects.toEqual(new AppError("E-mail ou senha incorreta"));
+  });
+});
